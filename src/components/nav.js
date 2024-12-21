af12d85578280f6d92477de45bc373eda1594c3d
NurulAqilahAhmad00 <nrlaqilahahmd@gmail.com>
2024-12-21 09:55:49 +0800

    Responsive Navigation Menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import './nav.css';
 import { Link, NavLink } from "react-router-dom";
 import logo from '../aqilah-logo.png';
 import { Text } from "@chakra-ui/react";
 import { IconButton } from "@chakra-ui/react"
-import { IoInformationCircleOutline } from "react-icons/io5";
-import { IoListCircleOutline } from "react-icons/io5";
+import { IoListCircleOutline, IoInformationCircleOutline } from "react-icons/io5";
 import { HiOutlineUserCircle } from "react-icons/hi2";
+import { IoMdClose } from "react-icons/io";
+import { RiMenu5Fill } from "react-icons/ri";
 
 export const Nav = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
         <nav className="flex justify-between align-center px-10 py-8 sticky top-0">
             <Link to="/">
                 <div className="rounded-xl border-4 border-[#D09CFA]"><Text className="text-2xl font-bold px-3 py-2" color="#D09CFA">Q</Text></div>
             </Link>
-            <div className="flex absolute">
-                <span></span>
-                <span></span>
-                <span></span>
+            <div className="menu" onClick={() => setOpenMenu(!openMenu) }>
+                <IconButton color="#D09CFA" aria-label="Menu">
+                    <RiMenu5Fill />
+                </IconButton>
             </div>
-            <ul className="flex px-3 py-3 gap-6">
+            <ul className={openMenu ? "open" : ""}>
+                <li className="close-menu">
+                    <IconButton aria-label="Close" onClick={() => setOpenMenu(false) }>
+                        <IoMdClose />
+                    </IconButton>
+                </li>
                 <li>
                     <NavLink className="duration-500" to="/about"><IconButton color="#D09CFA" aria-label="About">
                         <HiOutlineUserCircle />
