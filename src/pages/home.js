57914ad20482c9fed8484bf8c3c37f4a0d7956f4
NurulAqilahAhmad00 <nrlaqilahahmd@gmail.com>
2024-12-22 10:08:14 +0800

    Update Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,24 +2,25 @@ import React from "react";
 import { Text, HStack } from "@chakra-ui/react";
 import { Button } from "../components/ui/button";
 import '../index.css';
-import { Link, NavLink } from "react-router-dom";
+import './styles/home.css';
+import { NavLink } from "react-router-dom";
 
 export const Home = () => {
     return (
 
         <div className="px-10">
             <title>Home – Nurul Aqilah Ahmad</title>
-            <div className="flex flex-col justify-center items-start text-left px-40 py-40 gap-10">
-                <div>
+            <div className="flex flex-col justify-center items-start text-left inset-x-full py-40 gap-10">
+                <div className="primary-flex">
                     <Text className="text-xl font-bold" color="#7D12FF">Hello, I'm</Text>
                 </div>
-                <div>
+                <div className="primary-flex">
                     <Text className="text-6xl font-bold">Nurul Aqilah Ahmad</Text>
                 </div>
-                <div>
+                <div className="primary-flex">
                     <Text className="text-xl">A motivated and dedicated individual with a passion for programming seeks hands-on experience with a team of professionals. Strong foundation in software development concepts and eagerness to learn new technologies.</Text>
                 </div>
-                <div>
+                <div className="primary-flex">
                     <NavLink className="duration-500" to="/about"><HStack>
                         <Button className="primary-btn px-3">Know More</Button>
                     </HStack></NavLink>
