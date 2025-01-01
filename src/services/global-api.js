0bc288ac05aeec44124bd1fec3a48bfa9fb247bd
NurulAqilahAhmad00 <nrlaqilahahmd@gmail.com>
2025-01-01 18:08:11 +0800

    Install GraphQL

---
+++ src/services/global-api.js
@@ -0,0 +1,29 @@
+const { gql, default: request } = require("graphql-request")
+
+const MASTER_URL = 'https://ap-south-1.cdn.hygraph.com/content/'+process.env.PUBLIC_MASTER_URL_KEY+'/master'
+
+const getEmploymentHistory = async() => {
+    const query = gql `
+    query EmploymentHistory {
+        employmentHistories {
+          id
+          position
+          companyName
+          startDate
+          endDate
+          workDescription {
+            id
+            explanation
+          }
+        }
+      }
+      `
+
+      const result = await request(MASTER_URL, query)
+
+      return result
+}
+
+export default {
+    getEmploymentHistory
+}
