e4801989be8976b35640bb05c5643e22d5d9cbc3
nurulaqilahahmad <nrlaqilahahmd@gmail.com>
2025-01-09 16:10:47 +0800

    Add Services section in Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,7 @@ import { Button } from "../components/ui/button";
 import '../index.css';
 import './styles/home.css';
 import { NavLink } from "react-router-dom";
+import { ImArrowUpRight2 } from "react-icons/im";
 
 export const Home = () => {
 
@@ -46,52 +47,60 @@ export const Home = () => {
                 </div>
             </section>
 
-            <section className="flex flex-col justify-center items-center text-left py-40 gap-10 animate">
+            <section className="flex flex-col justify-center items-center text-left py-40 gap-10">
                 <div className="flex flex-col gap-10 primary-flex">
                     <div className="flex flex-row justify-between items-center gap-5 w-full">
                         <hr className="w-full border-violet-400"></hr>
-                        <Text className="text-nowrap font-bold" color="#7D12FF">Brief Intro</Text>
+                        <Text className="text-nowrap font-bold" color="#7D12FF">What Do I Do</Text>
                         <hr className="w-full border-violet-400"></hr>
                     </div>
-                    <Text className="description-text font-bold" color="#7D12FF">Hello World! My name is</Text>
-                    <Text className="text-6xl font-bold slideUpAnim">Nurul Aqilah Ahmad<span className="text-[#7D12FF]">.</span></Text>
-                    <Text className="description-text">A motivated and dedicated individual with a passion for programming seeks hands-on experience with a team of professionals. Strong foundation in software development concepts and eagerness to learn new technologies.</Text>
-                    <Text className="description-text">Currently a Software Developer at <a href="https://www.milradius.com.my/" className="link">Millennium Radius</a>.</Text>
-                    <NavLink className="duration-500" to="/projects"><HStack>
-                        <Button className="primary-btn px-3">Check out my projects!</Button>
-                    </HStack></NavLink>
-                </div>
-            </section>
-
-            <section className="flex flex-col justify-center items-center text-left py-40 gap-10">
-                <div className="flex flex-col gap-10">
-                    <div className="flex flex-row justify-between items-center gap-5 w-full">
-                        <Text className="text-nowrap font-bold" color="#7D12FF">Brief Intro</Text>
-                        <hr className="w-full text-white"></hr>
+                    <div className="flex lg:flex-row flex-col gap-10 primary-flex w-full flex-wrap">
+                        <div className="flex flex-col flex-box gap-10 hover:scale-110 duration-[0.4s] md:min-h-96" style={{ flex: '40%' }}>
+                            <Text className="text-xl font-bold"><span className="text-[#7D12FF]">01. </span>Web</Text>
+                            <Text className="flex grow">Built web applications that includes CRUD operations to cater stakeholders' needs.</Text>
+                            <div className="flex justify-end">
+                                <NavLink className="duration-500" to="/projects" style={{ display: 'inline-block', width: 'fit-content' }}><HStack>
+                                    <Button className="primary-btn px-4 py-4"><ImArrowUpRight2 /></Button>
+                                </HStack></NavLink>
+                            </div>
+                        </div>
+                        <div className="flex flex-col flex-box gap-10 hover:scale-110 duration-[0.4s] md:min-h-96" style={{ flex: '40%' }}>
+                            <Text className="text-xl font-bold"><span className="text-[#7D12FF]">02. </span>Mobile</Text>
+                            <Text className="flex grow">Developed mobile applications for portable use.</Text>
+                            <div className="flex justify-end">
+                                <NavLink className="duration-500" to="/projects" style={{ display: 'inline-block', width: 'fit-content' }}><HStack>
+                                    <Button className="primary-btn px-4 py-4"><ImArrowUpRight2 /></Button>
+                                </HStack></NavLink>
+                            </div>
+                        </div>
+                        <div className="flex flex-col flex-box gap-10 hover:scale-110 duration-[0.4s] md:min-h-96" style={{ flex: '40%' }}>
+                            <Text className="text-xl font-bold"><span className="text-[#7D12FF]">03. </span>Game</Text>
+                            <Text className="flex grow">Focused on game elements and code its functionality to build fun.</Text>
+                            <div className="flex justify-end">
+                                <NavLink className="duration-500" to="/projects" style={{ display: 'inline-block', width: 'fit-content' }}><HStack>
+                                    <Button className="primary-btn px-4 py-4"><ImArrowUpRight2 /></Button>
+                                </HStack></NavLink>
+                            </div>
+                        </div>
+                        <div className="flex flex-col flex-box gap-10 hover:scale-110 duration-[0.4s] md:min-h-96" style={{ flex: '40%' }}>
+                            <Text className="text-xl font-bold"><span className="text-[#7D12FF]">04. </span>VR / AR</Text>
+                            <Text className="flex grow">Created virtual environment to be able to gain rare experiences.</Text>
+                            <div className="flex justify-end">
+                                <NavLink className="duration-500" to="/projects" style={{ display: 'inline-block', width: 'fit-content' }}><HStack>
+                                    <Button className="primary-btn px-4 py-4"><ImArrowUpRight2 /></Button>
+                                </HStack></NavLink>
+                            </div>
+                        </div>
+                        <div className="flex flex-col flex-box gap-10 hover:scale-110 duration-[0.4s] md:min-h-96" style={{ flex: '40%' }}>
+                            <Text className="text-xl font-bold"><span className="text-[#7D12FF]">05. </span>Design</Text>
+                            <Text className="flex grow">Brainstormed design ideas that follow stakeholders' needs while complying usability.</Text>
+                            <div className="flex justify-end">
+                                <NavLink className="duration-500" to="/projects" style={{ display: 'inline-block', width: 'fit-content' }}><HStack>
+                                    <Button className="primary-btn px-4 py-4"><ImArrowUpRight2 /></Button>
+                                </HStack></NavLink>
+                            </div>
+                        </div>
                     </div>
-                    <Text className="description-text font-bold" color="#7D12FF">Hello World! My name is</Text>
-                    <Text className="text-6xl font-bold slideUpAnim">Nurul Aqilah Ahmad<span className="text-[#7D12FF]">.</span></Text>
-                    <Text className="description-text">A motivated and dedicated individual with a passion for programming seeks hands-on experience with a team of professionals. Strong foundation in software development concepts and eagerness to learn new technologies.</Text>
-                    <Text className="description-text">Currently a Software Developer at <a href="https://www.milradius.com.my/" className="link">Millennium Radius</a>.</Text>
-                    <NavLink className="duration-500" to="/projects"><HStack>
-                        <Button className="primary-btn px-3">Check out my projects!</Button>
-                    </HStack></NavLink>
-                </div>
-            </section>
-
-            <section className="flex flex-col justify-center items-center text-left py-40 gap-10">
-                <div className="flex flex-col gap-10">
-                    <div className="flex flex-row justify-between items-center gap-5 w-full">
-                        <hr className="w-full text-white"></hr>
-                        <Text className="text-nowrap font-bold" color="#7D12FF">Brief Intro</Text>
-                    </div>
-                    <Text className="description-text font-bold" color="#7D12FF">Hello World! My name is</Text>
-                    <Text className="text-6xl font-bold slideUpAnim">Nurul Aqilah Ahmad<span className="text-[#7D12FF]">.</span></Text>
-                    <Text className="description-text">A motivated and dedicated individual with a passion for programming seeks hands-on experience with a team of professionals. Strong foundation in software development concepts and eagerness to learn new technologies.</Text>
-                    <Text className="description-text">Currently a Software Developer at <a href="https://www.milradius.com.my/" className="link">Millennium Radius</a>.</Text>
-                    <NavLink className="duration-500" to="/projects"><HStack>
-                        <Button className="primary-btn px-3">Check out my projects!</Button>
-                    </HStack></NavLink>
                 </div>
             </section>
 
