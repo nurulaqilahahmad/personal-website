0e6b716010907269eec8a8f71b81ed9f775c6aba
nurulaqilahahmad <nrlaqilahahmd@gmail.com>
2025-02-19 09:53:07 +0800

    Modify Contact page to add Contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../index.css';
 import { Text, HStack } from "@chakra-ui/react";
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
-import { HiOutlineLocationMarker, HiOutlineMail, HiOutlineUsers } from "react-icons/hi";
+import { HiInboxIn, HiOutlineLocationMarker, HiOutlineMail, HiOutlineUsers } from "react-icons/hi";
 
 export const Contact = () => {
     const [title] = useTypewriter({
@@ -19,8 +19,16 @@ export const Contact = () => {
             <Text className="title">{title}<Cursor cursorColor="#7D12FF" /></Text>
 
             <section className="flex flex-col justify-center items-center text-left pt-10 pb-[5.4rem] gap-10">
-                <div className="flex flex-col gap-10 w-[60%]">
-                    <div className="flex lg:flex-row flex-col gap-10 primary-flex w-full flex-wrap">
+                <div className="flex lg:flex-row flex-col gap-10 w-[60%]">
+                    {/* Contact Form */}
+                    <div className="flex flex-col gap-10 primary-flex w-full flex-wrap">
+                        <div className="flex flex-col flex-box-primary gap-10 hover:scale-110 duration-[0.4s] md:min-h-96 p-10 w-full" style={{ flex: '5%' }}>
+                            <HiInboxIn className="text-[#7D12FF] text-5xl" />
+                            <Text className="">I am based in Sungai Buloh, Selangor.</Text>
+                        </div>
+                    </div>
+                    {/* Additional Details */}
+                    <div className="flex flex-col gap-10 primary-flex w-full flex-wrap">
                         <div className="flex flex-col flex-box-primary gap-10 hover:scale-110 duration-[0.4s] md:min-h-96 p-10 w-full" style={{ flex: '5%' }}>
                             <HiOutlineLocationMarker className="text-[#7D12FF] text-5xl" />
                             <Text className="">I am based in Sungai Buloh, Selangor.</Text>
@@ -31,7 +39,7 @@ export const Contact = () => {
                             <Text className="">Email me at <a href="mailto:nrlaqilahahmd@gmail.com" className="link">nrlaqilahahmd@gmail.com</a>.</Text>
                         </div>
                         <div className="flex flex-col flex-box-primary gap-10 hover:scale-110 duration-[0.4s] md:min-h-96 p-10 w-full" style={{ flex: '5%' }}>
-                            <HiOutlineUsers  className="text-[#7D12FF] text-5xl" />
+                            <HiOutlineUsers className="text-[#7D12FF] text-5xl" />
                             <Text className="">Let's Connect through my socials:</Text>
                             <Text className=""><a href="https://www.linkedin.com/in/nurul-aqilah-ahmad-604b90216/" target="_blank" className="link">LinkedIn</a></Text>
                             <Text className=""><a href="https://github.com/nurulaqilahahmad" target="_blank" className="link">GitHub</a></Text>
